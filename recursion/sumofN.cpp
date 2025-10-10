05820f20294a4fc83dc1092b7e07cde6ecd861e2
SohamDas00 <jisusoham04@gmail.com>
2025-10-10 21:23:19 +0530

    sum of array till N using recursion

--- recursion/sumofN.cpp
+++ recursion/sumofN.cpp
@@ -1,11 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+int sum(int num,int n,int sums){
+    if(n>num) return sums;
+    sums+=n;
+    sum(num,n+=1,sums);
+}
 
 int main(){
     int num;
     cout<<"Enter num: ";
     cin>>num;
-    sum(num);
+    cout<<"Sum: "<<sum(num,1,0);
 }
